d7b8cee67b3a5cfc64ca21cf9db1f1317b4f1a0f
Kåre Karlsen <karek@PC-KAREK>
2018-04-26 13:20:17 +0200

    add missing client assertion classes

---
+++ HelseId.Common/ClientAssertion/ClientAssertion.cs
@@ -0,0 +1,39 @@
+﻿using HelseId.Common.Certificates;	
+using HelseId.Common.Crypto;	
+using IdentityModel;	
+using Microsoft.IdentityModel.Tokens;	
+	
+	
+namespace HelseId.Common.ClientAssertion
+{	
+    public class ClientAssertion
+    {	
+        public string Assertion { get; set; }	
+	
+        public string AssertionType { get; set; } = OidcConstants.ClientAssertionTypes.JwtBearer;	
+	
+        public static ClientAssertion CreateWithRsaKeys(string clientId, string tokenEndpointUrl)
+        {	
+            var rsa = RSAKeyGenerator.GetRsaParameters(clientId);	
+            var securityKey = new RsaSecurityKey(rsa);	
+            var assertion = JwtGenerator.Generate(clientId, tokenEndpointUrl, JwtGenerator.SigningMethod.RsaSecurityKey,
+securityKey);	
+	
+            return new ClientAssertion
+            {
+                Assertion = assertion
+            };	
+        }	
+	
+        public static ClientAssertion CreateWithEnterpriseCertificate(string clientId, string tokenEndpointUrl,
+            string thumbprint)
+        {	
+            var certificate = CertificateStore.GetCertificateByThumbprint(thumbprint);	
+            var securityKey = new X509SecurityKey(certificate);	
+            var assertion = JwtGenerator.Generate(clientId, tokenEndpointUrl,
+JwtGenerator.SigningMethod.X509EnterpriseSecurityKey, securityKey);	
+	
+            return new ClientAssertion { Assertion = assertion };	
+        }	
+    }	
+} 
